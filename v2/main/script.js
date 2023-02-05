998c6caf30e1d2c5014d1733fc00b932b4534f2b
Nikolaj Kim <nikolajk@uci.edu>
2023-02-04 23:54:45 -0800

    print shows up when i add songs

--- v2/main/script.js
+++ v2/main/script.js
@@ -1,4 +1,21 @@
 let access_token = '';
+let playlist_id = '';
+let songData = {
+    "uris": ["spotify:track:4iV5W9uYEdYUVa79Axb7Rh"]
+  };
+let message = "coding is religion".split(" ")
+message.reverse()
+
+// function getInputValue(){
+//     // Selecting the input element and get its value 
+//     var inputVal = document.getElementById("sentence").value;
+//     message = inputVal.split(" ")
+//     message.reverse();
+//     console.log();
+
+    
+
+// }
 
 function load() {
     console.log(window.location);
@@ -18,11 +35,14 @@ function load() {
         }
 
     ).then(res => res.json()).then(renderSpotifyDataInHTML);
-    
-    
 }
 
 function renderSpotifyDataInHTML(spotifyData) {
+    console.log(spotifyData);
+    document.body.innerHTML += spotifyData.display_name;
+    id = spotifyData.display_name
+    document.body.innerHTML += `<img src="${spotifyData.images[0].url}" />`;
+
 
     let playlistData = {
         "name": "Love List",
@@ -41,16 +61,61 @@ function renderSpotifyDataInHTML(spotifyData) {
                 console.log(response);
               }
         }
-    ).then(res => res.json())
+    ).then(res => res.json()).then(savePlayListData)
 
-    console.log(spotifyData);
-    document.body.innerHTML += spotifyData.display_name;
-    id = spotifyData.display_name
-    document.body.innerHTML += `<img src="${spotifyData.images[0].url}" />`;
-}
+    function savePlayListData(playlist_info){
+        playlist_id = playlist_info.id
 
-function createPlaylist(spotifyData){
-    //console.log(spotifyData)
+        function processSearch(searchResults){
+            // songData.push( searchResults.track.items[0].uri)
+
+            let temp = {"uris": [searchResults.tracks.items[0].uri], "position": 0}
+            addSongsToPlaylist(playlist_id, temp)
+
+        }
+
+        for (let i = 0; i <= message.length-1; i++) {
+            fetch('https://api.spotify.com/v1/search?q=' + message[i] + '&type=track',
+                {
+                    method: 'GET',
+                    headers: new Headers({
+                        'Authorization': 'Bearer ' + access_token,
+                        'Content-Type': 'application/json'
+                    }),
+                    success: function(response) {
+                        console.log(response);
+                      }
+                }
+            ).then(res => res.json()).then(processSearch)
+            
+          }
+
+         
+    }
+
+    function addSongsToPlaylist(playlist_id, songData){
+        // if(songs.length) return;
+        // let songData = songs[0];
+        print(playlist_id)
+        uri = JSON.stringify(songData)
+
+        fetch('https://api.spotify.com/v1/playlists/' + playlist_id + '/tracks',
+        {
+            method: 'POST',
+            body: JSON.stringify(songData),
+            headers: new Headers({
+                'Authorization': 'Bearer ' + access_token,
+                'Content-Type': 'application/json'
+            }),
+            success: function(response) {
+                console.log(response);
+              }
+        }
+    ).then(res => res.json()).then()
+    }
+
+    
+
 }
 
 
